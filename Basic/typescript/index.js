db99bedf6e84b9f8cc5362125692a5e0fdb8b245
Natanael Lucena de Medeiros <nldm.eng20@uea.edu.br>
2021-07-11 10:25:06 -0400

    add typescript lesson

---
+++ Basic/typescript/index.js
@@ -0,0 +1,3 @@
+"use strict";
+var somar = function (num1, num2) { return num1 + num2; };
+console.log(somar(3, 6));
---
+++ Basic/typescript/index.ts
@@ -0,0 +1,2 @@
+const somar = (num1: number, num2: number) => num1+num2;
+console.log(somar(3, 6));
