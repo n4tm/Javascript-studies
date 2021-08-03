52eea6fa13327520820f49d632779360277732a3
Natanael Lucena <nldm.eng20@uea.edu.br>
2021-08-03 15:55:50 -0400

    Implement Express.js into back-end

---
+++ Basic/GraphQL/dev-demands/packages/server/src/main.js
@@ -0,0 +1,20 @@
+import express from 'express';
+
+const server = express();
+server.get('/status', (_, response) => {
+    response.send({
+        status: 'Okay'
+    });
+});
+
+server.post('/authenticate', express.json(), (request, response) => {
+    console.log('E-mail', request.body.email, 'Senha', request.body.password);
+    response.send();
+});
+
+const PORT = process.env.PORT ? parseInt(process.env.PORT) : 8000;
+const HOSTNAME = process.env.HOSTNAME || server.address().address;
+
+server.listen(PORT, HOSTNAME, () => {
+    console.log(`Server is listening at http://${HOSTNAME}:${PORT}.`)
+});
